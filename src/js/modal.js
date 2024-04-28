6d8edd787e53e647bb102c655f7516c3084ba21a
Bohdan <bromanenko333@gmail.com>
2024-04-29 00:25:23 +0300

    Adding modal windows and fixing buttons click on mobile devices

---
+++ src/js/modal.js
@@ -0,0 +1,40 @@
+const overlay = document.querySelector('.overlay');
+const consultationModal = document.querySelector('#consultation');
+const orderModal = document.querySelector('#order');
+const gratitudeModal = document.querySelector('#gratitude');
+
+const consultationButton = document.querySelector('.button_main');
+consultationButton.addEventListener('click', () => {
+    overlay.style.display = 'block';
+    consultationModal.style.display = 'block';
+});
+
+const consultationThnaksButton = consultationModal.querySelector('.button_submit');
+consultationThnaksButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    overlay.style.display = 'block';
+    consultationModal.style.display = 'none';
+    gratitudeModal.style.display = 'block';
+});
+
+const orderButtons = document.querySelectorAll('.button_mini');
+orderButtons.forEach(orderButton => {
+    orderButton.addEventListener('click', () => {
+        overlay.style.display = 'block';
+    orderModal.style.display = 'block';
+    });
+});
+
+const thanksButton = document.querySelector('.button_submit');
+thanksButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    overlay.style.display = 'block';
+    gratitudeModal.style.display = 'block';
+});
+
+overlay.addEventListener('click', (event) => {
+    if (event.target.classList.contains('modal__close')) {
+        overlay.style.display = 'none';
+        event.target.closest('.modal').style.display = 'none';
+    }
+});
